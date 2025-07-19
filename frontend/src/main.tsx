82f23962ae11fbe2ed11d386d0a230b26d0f5cbd
lizka <lizka4231@gmail.com>
2025-07-19 15:40:31 +0300

    feat: add reset styles

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,6 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './app/App.tsx'
 
+import '@/styles/reset.scss';
+
 createRoot(document.getElementById('root')!).render(
    <StrictMode>
       <App />

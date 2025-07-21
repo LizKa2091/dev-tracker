04e2687d3b7f5dffe67dd27c42f49a453cc45243
lizka <lizka4231@gmail.com>
2025-07-21 20:40:38 +0300

    feat(new note): add form errors handling

--- frontend/src/features/notes/NewNoteForm.tsx
+++ frontend/src/features/notes/NewNoteForm.tsx
@@ -31,24 +31,30 @@ const tagOptions = [
 ];
 
 const NewNoteForm: FC = () => {
-   const { handleSubmit, control } = useForm<NewNoteFormData>();
+   const { handleSubmit, control, formState: { errors }, trigger } = useForm<NewNoteFormData>();
 
    const onSubmit = (data: NewNoteFormData): void => {
       console.log(data);
    };
 
+   const onFinish = async (): Promise<void> => {
+      const isValidData = await trigger(['title', 'type', 'date']);
+
+      if (isValidData) handleSubmit(onSubmit)();
+   };
+
    return (
-      <Form onFinish={handleSubmit(onSubmit)}>
-         <Form.Item label='Название' className={styles.formItem}>
+      <Form onFinish={onFinish}>
+         <Form.Item label='Название' required validateStatus={errors.title ? 'error' : ''} help={errors.title && 'Обязательное поле'} className={styles.formItem}>
             <Controller name='title' control={control} rules={{ required: true }} render={({ field }) => <Input {...field} />} />
          </Form.Item>
-         <Form.Item label='Тип записи' className={styles.formItem}>
+         <Form.Item label='Тип записи' required validateStatus={errors.type ? 'error' : ''} help={errors.type && 'Обязательное поле'} className={styles.formItem}>
             <Controller name='type' control={control} rules={{ required: true }} render={({ field }) => <Select showSearch options={typeOptions} {...field} />} />
          </Form.Item>
          <Form.Item label='Теги' className={styles.formItem}>
             <Controller name='tags' control={control} render={({ field }) => <Select mode='tags' options={tagOptions} className={styles.formTags} {...field} />} />
          </Form.Item>
-         <Form.Item label='Выполнить до' className={styles.formItem}>
+         <Form.Item label='Выполнить до' required validateStatus={errors.date ? 'error' : ''} help={errors.date && 'Обязательное поле'} className={styles.formItem}>
             <Controller name='date' control={control} rules={{ required: true }} render={({ field }) => <DatePicker {...field} />} />
          </Form.Item>
          <Form.Item label='Описание' className={styles.formItem}>

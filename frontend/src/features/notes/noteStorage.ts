a8fd24ac2c8813b101265b8da7923dd231a7b153
lizka <lizka4231@gmail.com>
2025-07-22 21:26:36 +0300

    feat(notes): add note storage functions

---
+++ frontend/src/features/notes/noteStorage.ts
@@ -0,0 +1,20 @@
+import type { INoteItem } from "./noteTypes";
+
+interface INotesStorage {
+   notes: INoteItem[] | null;
+}
+
+export const saveNote = (noteData: INoteItem): INotesStorage => {
+   const savedNotesData = loadNotes().notes ?? [];
+
+   const notesToSave: INotesStorage = { notes: [...savedNotesData, noteData] };
+
+   localStorage.setItem('notes', JSON.stringify(notesToSave));
+   return notesToSave;
+};
+
+export const loadNotes = () => {
+   const notesData = localStorage.getItem('notes');
+
+   return notesData ? JSON.parse(notesData) : { notes: null };
+};

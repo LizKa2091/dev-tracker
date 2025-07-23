12d4147d68d1e3ce1a944faf799d5a98c860e23b
lizka <lizka4231@gmail.com>
2025-07-23 22:00:53 +0300

    feat(notes): add note item component

---
+++ frontend/src/shared/ui/NoteItem.tsx
@@ -0,0 +1,26 @@
+import { type FC } from 'react'
+import type { INoteItem } from '../../features/notes/noteTypes';
+import { Badge, Card, Space, Tag } from 'antd';
+import styles from './NoteItem.module.scss';
+
+interface INoteItemProps {
+   noteItemData: INoteItem;
+};
+
+const NoteItem: FC<INoteItemProps> = ({ noteItemData }) => {
+   return (
+      <Card title={noteItemData.title} extra={<Tag color='blue'>{noteItemData.type}</Tag>} className={noteItemData.tags?.length ? styles.card : ''}>
+         <Space direction='vertical'>
+            <p className={styles.cardDetail}>{noteItemData.description || 'Нет описания'}</p>
+            <p className={styles.cardDetail}>Выполнить до: {noteItemData.date}</p>
+            <Space>
+               {noteItemData.tags?.map(tag => 
+                  <Badge key={tag} count={tag} text={tag} />
+               )}
+            </Space>
+         </Space>
+      </Card>
+   )
+}
+
+export default NoteItem;

4155285842b10e9d00cb4472f2a4f79ac3081807
lizka <lizka4231@gmail.com>
2025-07-25 22:51:42 +0300

    feat(note storage): add function for updating note data

--- frontend/src/features/notes/model/noteStorage.ts
+++ frontend/src/features/notes/model/noteStorage.ts
@@ -23,4 +23,16 @@ export const loadRecentNotes = (amount: number = 5): LoadRecentNotesResponse => 
    const notesData = loadNotes().notes;
 
    return { notes: notesData?.slice(-amount).reverse() ?? null, limit: notesData?.length ?? 0 };
+};
+
+export const updateNote = (noteData: INoteItem) => {
+   const savedNotes = loadNotes().notes ?? [];
+
+   const updatedNotes = savedNotes.map(note => note.key === noteData.key ? noteData : note);
+
+   const notesToSave = {notes: updatedNotes};
+
+   localStorage.setItem('notes', JSON.stringify(notesToSave));
+   console.log(notesToSave);
+   return notesToSave;
 }

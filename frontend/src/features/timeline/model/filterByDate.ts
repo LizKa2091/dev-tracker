61beb9e12a2882325a2c994d00eec2bc9cd07a15
Liza <lizka4231@gmail.com>
2025-08-11 16:09:38 +0300

    feat(timeline): add function for filtering notes by date

---
+++ frontend/src/features/timeline/model/filterByDate.ts
@@ -0,0 +1,32 @@
+import type { INoteItem } from "../../notes/noteTypes";
+import dayjs from 'dayjs';
+
+dayjs().format()
+
+export const filterByDate = (segment: string): INoteItem[] => {
+   let destinDay: dayjs.Dayjs;
+
+   switch (segment) {
+      case 'day':
+         destinDay = dayjs().subtract(1, 'day');
+         break;
+      case 'week':
+         destinDay = dayjs().subtract(7, 'day');
+         break;
+      case 'month':
+         destinDay = dayjs().subtract(30, 'day');
+         break;
+      case 'year':
+         destinDay = dayjs().subtract(365, 'day');
+         break;
+      default:
+         return [];
+   }
+
+   const savedNotes = localStorage.getItem('notes');
+   if (!savedNotes) return [];
+
+   const filteredNotes: INoteItem[] = JSON.parse(savedNotes).notes.filter((note: INoteItem) => dayjs(note.date).isAfter(destinDay));
+
+   return filteredNotes;
+};

922ed2554425fe1bafcfbb8a8b3b42e14e3ab4c0
lizka <lizka4231@gmail.com>
2025-08-16 20:30:14 +0300

    feat(tags settings): add tags settings, implement loading user tags

--- frontend/src/features/settings/ui/Tags/TagsSettings.tsx
+++ frontend/src/features/settings/ui/Tags/TagsSettings.tsx
@@ -1,15 +1,31 @@
-import { useState, type FC } from 'react'
+import { useEffect, useState, type FC } from 'react'
 import AddCustomTag from './AddCustomTag';
-import { Button } from 'antd';
+import { Button, Flex, Tag } from 'antd';
+import { type ITagItem } from '../../tagTypes';
+import { deleteUserTag, loadUserTags } from '../../model/tagActions';
+import styles from './TagsSettings.module.scss';
 
 const TagsSettings: FC = () => {
    const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+   const [savedTags, setSavedTags] = useState<ITagItem[]>();
+
+   useEffect(() => {
+      setSavedTags(loadUserTags());
+   }, []);
 
    return (
-      <>
-         <Button color="default" variant="solid" onClick={() => setIsModalOpen(true)}>Добавить новый тэг</Button>
-         <AddCustomTag isModalOpen={isModalOpen} setIsModalOpen={setIsModalOpen} />
-      </>
+      <Flex vertical gap='large' className={styles.container}>
+         <h3>Тэги</h3>
+         <Flex vertical gap='large' className={styles.innerContainer}>
+            <Flex>
+               {savedTags?.map(tag => (
+                  <Tag key={tag.key} color={tag.color} closable onClose={() => deleteUserTag(tag.label)}>{tag.label}</Tag>
+               ))}
+            </Flex>
+            <Button color="default" variant="solid" onClick={() => setIsModalOpen(true)}>Добавить новый тэг</Button>
+            <AddCustomTag isModalOpen={isModalOpen} setIsModalOpen={setIsModalOpen} />
+         </Flex>
+      </Flex>
    )
 }
 

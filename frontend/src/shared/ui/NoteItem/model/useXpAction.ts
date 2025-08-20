83faf2b77c998fded5f7b4b8d50b84ac0b72f6a1
lizka <lizka4231@gmail.com>
2025-08-20 11:45:28 +0300

    feat(use xp action): update request to match backend changes

--- frontend/src/shared/ui/NoteItem/model/useXpAction.ts
+++ frontend/src/shared/ui/NoteItem/model/useXpAction.ts
@@ -9,26 +9,31 @@ interface IXpActionResponse {
    xpForNextLevel?: number;
 };
 
-const xpAction = async (action: actionValues, amount: number, token: string): Promise<IXpActionResponse> => {
+const xpAction = async (action: actionValues, token: string): Promise<IXpActionResponse> => {
    const response = await fetch(`http://localhost:5001/xp/${action}`, {
       method: 'POST',
       headers: {
          'Content-Type': 'application/json',
          'authorization': `Bearer ${token}`
-      },
-      body: JSON.stringify({ amount })
+      }
    });
 
-   if (!response.ok) throw new Error('ошибка сервера');
-
    const result = await response.json();
+   if (!response.ok) throw new Error(result.message || 'ошибка сервера');
+
    return result;
 };
 
-export const useXpAction = (action: actionValues) => {
-   return useMutation<IXpActionResponse, Error, { amount: number; token: string }>({
-      mutationKey: [`xp ${action}`],
-      mutationFn: ({ amount, token }) => xpAction(action, amount, token),
+export const useXpAction = (token: string | null) => {
+   const mutation = useMutation<IXpActionResponse, Error, actionValues>({
+      mutationKey: ['xpAction', token],
+      mutationFn: async (action) => {
+         if (!token) throw new Error('не предоставлен токен');
+
+         return await xpAction(action, token)
+      },
       retry: 1
-   })
+   });
+
+   return { ...mutation, addXp: () => mutation.mutateAsync('add'), removeXp: () => mutation.mutateAsync('remove') };
 };

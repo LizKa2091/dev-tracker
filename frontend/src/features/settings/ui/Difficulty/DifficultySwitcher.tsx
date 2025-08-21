5a8f4a37e1324f706f864c459b74c9d38e16af92
lizka <lizka4231@gmail.com>
2025-08-21 12:23:09 +0300

    feat: add difficulty settings

---
+++ frontend/src/features/settings/ui/Difficulty/DifficultySwitcher.tsx
@@ -0,0 +1,40 @@
+import { Flex, Switch } from 'antd';
+import { type FC } from 'react'
+import { useUserData } from '../../../user/model/useUserData';
+import { useUpdateDifficulty } from '../../model/useUpdateDifficulty';
+import { useQueryClient } from '@tanstack/react-query';
+import type { Difficulty } from '../../../user/userTypes';
+
+const DifficultySwitcher: FC = () => {
+   const token = localStorage.getItem('token');
+   const { data: userData } = useUserData(token);
+   const { mutate: updateDifficulty, isPending } = useUpdateDifficulty(token);
+
+   const queryClient = useQueryClient();
+
+   const handleChange = async (): Promise<void> => {
+      if (!token || !userData) return;
+
+      const newMode: Difficulty = userData.difficulty === 'default' ? 'hard' : 'default';
+      updateDifficulty({ difficulty: newMode }, {
+         onSuccess: () => {
+            queryClient.invalidateQueries({ queryKey: ['userData', token]});
+         }
+      });
+   };
+
+   return (
+      <Flex gap='middle' vertical>
+         <h4>Режим</h4>
+         <h5>Для более быстрого набора опыта выберите режим сложный, но в то же время невыполненные задания будут отнимать больше опыта, чем в обычном режиме</h5>
+         <Flex gap='middle'>
+            <span>Обычный</span>
+            <Switch checked={userData?.difficulty === 'hard'} onChange={handleChange} loading={isPending} />
+            <span>Сложный</span>
+         </Flex>
+         {}
+      </Flex>
+   )
+}
+
+export default DifficultySwitcher;

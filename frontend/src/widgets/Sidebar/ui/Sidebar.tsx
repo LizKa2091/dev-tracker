59e771971c69e57562026757af8b335db6f711d1
lizka <lizka4231@gmail.com>
2025-08-24 01:26:46 +0300

    feat(Sidebar): add home link, fix timeline link path

--- frontend/src/widgets/Sidebar/ui/Sidebar.tsx
+++ frontend/src/widgets/Sidebar/ui/Sidebar.tsx
@@ -3,14 +3,14 @@ import { Button, Flex, Layout, Menu } from 'antd';
 import { HistoryOutlined, FormOutlined, BarChartOutlined, SettingOutlined, LogoutOutlined, LoginOutlined } from "@ant-design/icons";
 import { Link, useNavigate } from 'react-router-dom';
 import AuthExports from '../../../shared/context/AuthContext';
-import styles from './Sidebar.module.scss';
 import UserProfile from '../../../features/user/ui/UserProfile';
+import styles from './Sidebar.module.scss';
 
 const { Sider } = Layout;
 const { useAuthContext } = AuthExports;
 
 const menuItems = [
-   { key: 'timeline', path: '/timeline', icon: <HistoryOutlined style={{ color: '#ffffff' }} />, label: <Link to='/'>Таймлайн</Link> },
+   { key: 'timeline', path: '/timeline', icon: <HistoryOutlined style={{ color: '#ffffff' }} />, label: <Link to='/timeline'>Таймлайн</Link> },
    { key: 'new post', path: '/new-post', icon: <FormOutlined style={{ color: '#ffffff' }} />, label: <Link to='/new-note'>Новая запись</Link> },
    { key: 'stats', icon: <BarChartOutlined style={{ color: '#ffffff' }} />, label: 'Статистика' },
    { key: 'settings', path: '/settings', icon: <SettingOutlined style={{ color: '#ffffff' }} />, label: <Link to='/settings'>Настройки</Link> }
@@ -32,7 +32,9 @@ const Sidebar: FC = () => {
    return (
       <Sider className={styles.sider}>
          <Flex vertical className={styles.siderContainer}>
-            <h1 className={styles.logo}>DevTracker</h1>
+            <h1 className={styles.logo}>
+               <Link to='/'>DevTracker</Link>
+            </h1>
             <UserProfile />
             <Menu items={menuItems} className={styles.menu} />
             {token ? (

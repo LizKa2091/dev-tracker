957cfc5c3e08dfe1be5fe8a4e60ac8422b6432e9
lizka <lizka4231@gmail.com>
2025-08-25 02:06:29 +0300

    feat(ResetPasswordPage): add page, add route

---
+++ frontend/src/pages/ResetPasswordPage/ResetPasswordPage.tsx
@@ -0,0 +1,25 @@
+import type { FC } from 'react';
+import Sidebar from '../../widgets/Sidebar/ui/Sidebar';
+import { Flex, Layout } from 'antd';
+import { Content } from 'antd/es/layout/layout';
+import FooterBar from '../../widgets/FooterBar/ui/FooterBar';
+import ResetPasswordForm from '../../features/auth/ui/ResetPasswordForm/ResetPasswordForm';
+
+const ResetPasswordPage: FC = () => {
+   return (
+      <Layout className='mainLayout'>
+         <Sidebar />
+         <Layout>
+            <Content className='content'>
+               <Flex vertical gap='large'>
+                  <h2 className='title'>Сброс пароля</h2>
+                  <ResetPasswordForm />
+               </Flex>
+            </Content>
+            <FooterBar />
+         </Layout>
+      </Layout>
+   )
+};
+
+export default ResetPasswordPage;

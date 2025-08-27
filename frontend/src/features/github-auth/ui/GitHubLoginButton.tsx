db9fe5d09fd0d257314f5e0a87ad659d5ba6eb34
lizka <lizka4231@gmail.com>
2025-08-27 15:15:31 +0300

    feat(githubloginbutton): add button

---
+++ frontend/src/features/github-auth/ui/GitHubLoginButton.tsx
@@ -0,0 +1,24 @@
+import type { FC } from 'react';
+import { GithubOutlined } from '@ant-design/icons';
+import { Button } from 'antd';
+import AuthExports from '../../../shared/context/AuthContext';
+import styles from './GitHubLoginButton.module.scss';
+
+const GitHubLoginButton: FC = () => {
+   const { token } = AuthExports.useAuthContext();
+   const githubToken = localStorage.getItem('githubToken');
+
+   const handleClick = () => {
+      window.location.href = 'http://localhost:5001/auth/github';
+   }
+
+   if (githubToken || !token) {
+      return null;
+   }
+
+   return (
+      <Button color="default" variant="solid" icon={<GithubOutlined />} onClick={handleClick} className={styles.button}>Связать с GitHub</Button>
+   )
+}
+
+export default GitHubLoginButton;

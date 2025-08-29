ed8a092e704704f42762614ac3ea8c300486b855
lizka <lizka4231@gmail.com>
2025-08-29 23:10:43 +0300

    feat(notifications context): add context

---
+++ frontend/src/shared/notifications/model/NotificationsContext.tsx
@@ -0,0 +1,46 @@
+import { createContext, useContext, useState, type FC, type ReactNode } from "react";
+import type { INotification } from "../notificationTypes";
+
+interface INotificationsContext {
+   notificationsData: INotification[];
+   addNotification: (notification: Omit<INotification, 'id'>) => void;
+   removeNotification: (id: number) => void;
+}
+
+interface INotificationsContextProvider {
+   children: ReactNode;
+}
+
+const NotificationsContext = createContext<INotificationsContext | undefined>(undefined);
+
+const NotificationsContextProvider: FC<INotificationsContextProvider> = ({ children }) => {
+   const [notificationsData, setNotificationsData] = useState<INotification[]>([]);
+
+   const addNotification = (notification: Omit<INotification, 'id'>): void => {
+      setNotificationsData(prev => {
+         const lastItemId = prev.length ? prev[prev.length-1].id : 0;
+         return [...prev, { ...notification, id: lastItemId+1 }]
+      });
+   };
+
+   const removeNotification = (id: number): void => {
+      setNotificationsData(prev => prev.filter(notif => notif.id !== id));
+   };
+
+   return (
+      <NotificationsContext.Provider value={{ notificationsData, addNotification, removeNotification }}>
+         {children}
+      </NotificationsContext.Provider>
+   )
+};
+
+const useNotifications = (): INotificationsContext => {
+   const context = useContext(NotificationsContext);
+   if (!context) throw new Error('useNotifications должен быть внутри контекста');
+
+   return context;
+};
+
+const NotificationsExports = { NotificationsContext, NotificationsContextProvider, useNotifications };
+
+export default NotificationsExports;

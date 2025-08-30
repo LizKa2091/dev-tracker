7c9e0b2ca52d1552a5ac116e0d70ecf2b0568f12
lizka <lizka4231@gmail.com>
2025-08-30 20:38:01 +0300

    feat(main): remove strict mode to avoid duplicate notifications

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,4 +1,3 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './app/App.tsx'
 
@@ -6,7 +5,5 @@ import '@/styles/reset.scss';
 import '@/styles/global.scss';
 
 createRoot(document.getElementById('root')!).render(
-   <StrictMode>
-      <App />
-   </StrictMode>,
+   <App />
 )

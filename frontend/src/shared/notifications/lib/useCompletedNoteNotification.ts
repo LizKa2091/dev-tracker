cb9b5b3f092e52e8c1182eafec3d964a03f441b8
Liza <lizka4231@gmail.com>
2025-09-09 11:54:17 +0300

    feat(notifications): add hook for notifying about completed note notifications

---
+++ frontend/src/shared/notifications/lib/useCompletedNoteNotification.ts
@@ -0,0 +1,35 @@
+import { useEffect, useState } from "react";
+import AuthExports from "../../context/AuthContext";
+import NotificationsExports from "../model/NotificationsContext"
+
+export const useCompletedNoteNotification = () => {
+   const { addNotification, removeNotification } = NotificationsExports.useNotifications();
+   const { checkLoginStatus } = AuthExports.useAuthContext();
+
+   const [difficulty, setDifficulty] = useState<'default' | 'hard' | null>(null);
+
+   useEffect(() => {
+      const getDifficulty = async (): Promise<void> => {
+         try {
+            const response = await checkLoginStatus();
+
+            setDifficulty(response.difficulty);
+         }
+         catch (err) {
+            console.error('ошибка при проверке сложности', err);
+         }
+      }
+
+      getDifficulty();
+   }, [checkLoginStatus]);
+
+   const notifyCompletedNote = (noteTitle: string, date: string) => {
+      if (difficulty === null) return;
+
+      const xpToGain: number = difficulty === 'hard' ? 12 : 20;
+
+      addNotification({ noteTitle, message: 'Выполнено задание', date, xp: xpToGain });
+   }
+
+   return { notifyCompletedNote, removeNotification };
+}

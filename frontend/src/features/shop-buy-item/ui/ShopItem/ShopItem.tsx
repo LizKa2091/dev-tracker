c6851c9455cbd922d276cfb2cdf4c11f6dbdccec
lizka <lizka4231@gmail.com>
2025-09-13 16:51:02 +0300

    feat(shop-buy-item): prevent purchase again if previous purchase was failed

--- frontend/src/features/shop-buy-item/ui/ShopItem/ShopItem.tsx
+++ frontend/src/features/shop-buy-item/ui/ShopItem/ShopItem.tsx
@@ -10,15 +10,14 @@ interface IShopItemProps {
    cost: number,
    description: string,
    image: string,
-   effect: any,
 };
 
-const ShopItem: FC<IShopItemProps> = ({ id, name, cost, description, image, effect }) => {
-   const { mutateAsync: buyItem, isPending } = useBuyItem();
+const ShopItem: FC<IShopItemProps> = ({ id, name, cost, description, image }) => {
+   const { mutateAsync: buyItem, isPending, isError } = useBuyItem();
    const { notifyPurchase } = usePurchaseNotification();
 
    const handleBuyItem = async () => {
-      if (isPending) return;
+      if (isPending || isError) return;
 
       try {
          const result = await buyItem({ itemId: id });
@@ -37,7 +36,7 @@ const ShopItem: FC<IShopItemProps> = ({ id, name, cost, description, image, effe
    return (
       <Flex className={styles.mainContainer}>
          <Tooltip title={description}>
-            <Flex align='center' vertical gap='middle' onClick={handleBuyItem} className={styles.itemContainer}>
+            <Flex align='center' vertical gap='middle' onClick={handleBuyItem} className={`${styles.itemContainer} ${isError ? 'disabled' : ''}`}>
                <p>{name}</p>
                <img src={`${import.meta.env.VITE_API_BASE_URL}${image}`} alt={name} />
                <p className={styles.price}>{cost}</p>

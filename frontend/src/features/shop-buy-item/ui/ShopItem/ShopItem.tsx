b9bc361cdd2df444277f16870d4d899e3f2351ca
lizka <lizka4231@gmail.com>
2025-09-13 16:07:24 +0300

    feat(shop-buy-item): add buying item

--- frontend/src/features/shop-buy-item/ui/ShopItem/ShopItem.tsx
+++ frontend/src/features/shop-buy-item/ui/ShopItem/ShopItem.tsx
@@ -1,5 +1,6 @@
 import type { FC } from 'react';
 import { Flex, Tooltip } from 'antd';
+import { useBuyItem } from '../../model/useBuyItem';
 import styles from './ShopItem.module.scss';
 
 interface IShopItemProps {
@@ -12,10 +13,18 @@ interface IShopItemProps {
 };
 
 const ShopItem: FC<IShopItemProps> = ({ id, name, cost, description, image, effect }) => {
+   const { mutate: buyItem, isPending } = useBuyItem();
+
+   const handleBuyItem = async () => {
+      if (isPending) return;
+
+      buyItem({ itemId: id });
+   };
+
    return (
       <Flex className={styles.mainContainer}>
          <Tooltip title={description}>
-            <Flex align='center' vertical gap='middle' className={styles.itemContainer}>
+            <Flex align='center' vertical gap='middle' onClick={handleBuyItem} className={styles.itemContainer}>
                <p>{name}</p>
                <img src={`${import.meta.env.VITE_API_BASE_URL}${image}`} alt={name} />
                <p className={styles.price}>{cost}</p>

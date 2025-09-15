5fb024f6912f5760639d4624d77815e5c9f21601
lizka <lizka4231@gmail.com>
2025-09-15 18:09:07 +0300

    feat(activity-cells): add hook for cells data

---
+++ frontend/src/features/activity-cells/model/useCellsData.ts
@@ -0,0 +1,73 @@
+import dayjs from "dayjs";
+import { useUserData } from '../../user/model/useUserData';
+import AuthExports from "../../../shared/context/AuthContext";
+import { githubLoadCommits } from "../../github/lib/githubStorage";
+import { useEffect, useState } from "react";
+
+interface ICellsData {
+   totalCells: number;
+   years: number[];
+   cellItems: ICellItemData[]
+};
+
+interface ICellItemData {
+   day: string;
+   activities: number;
+};
+
+export const useCellsData = () => {
+   const { token } = AuthExports.useAuthContext();
+   const { data: userData, isLoading } = useUserData(token);
+
+   const [cellsData, setCellsData] = useState<ICellsData>({
+      totalCells: 0,
+      years: [],
+      cellItems: []
+   });
+
+   useEffect(() => {
+      if (isLoading || !userData || !userData.registrationDate) return;
+
+      const today = dayjs();
+      const registerDate = dayjs(userData?.registrationDate);      
+
+      const years: number[] = [];
+      for (let i = registerDate.year(); i < today.year(); i++) {
+         years.push(i);
+      }
+
+      const commitMap = new Map<string, number>();
+
+      const savedCommits = githubLoadCommits();
+
+      savedCommits.repositories.forEach(rep => {
+         rep.commits.forEach(commit => {
+            const commitDay = dayjs(commit.author.date).startOf('day');
+            const registerDay = registerDate.startOf('day');
+
+            if (!commitDay.isBefore(registerDay)) {
+               const key = commitDay.toISOString();
+               commitMap.set(key, (commitMap.get(key) || 0) + 1);
+            }
+         });
+      });
+
+      const startDate = dayjs().startOf('year');
+
+      const totalCells: number = today.diff(startDate, 'day') + 1;     
+
+      const cellItems = Array.from({ length: totalCells }, (_, i) => {
+         const day = startDate.add(i, 'day');
+         const key = day.toISOString();
+         const registerDay = registerDate.startOf('day');
+
+         const activities = commitMap.get(key) || 0;
+
+         return { day: key, activities: day.isBefore(registerDay) ? 0 : activities };
+      });
+
+      setCellsData({ totalCells, years, cellItems });
+   }, [userData, isLoading]);
+
+   return cellsData;
+}

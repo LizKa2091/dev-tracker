fc940c1fd0ccf206b0a6d88a0c07e1591221fe64
lizka <lizka4231@gmail.com>
2025-09-25 18:19:34 +0300

    styles(HomePage and LandingPage): add adaptive layout

--- frontend/src/pages/HomePage/HomePage.tsx
+++ frontend/src/pages/HomePage/HomePage.tsx
@@ -2,16 +2,17 @@ import type { FC } from 'react';
 import { Flex } from 'antd';
 import MainLayout from '../../app/MainLayout';
 import DeadlineChart from '../../features/deadlines/ui/DeadlineChart';
+import styles from './HomePage.module.scss';
 
 const HomePage: FC = () => {
    return (
       <MainLayout>
-         <Flex vertical gap='large'>
-            <h2 className='title'>Главная</h2>
+         <Flex vertical gap="large" className={styles.container}>
+            <h2 className="title">Главная</h2>
             <DeadlineChart />
          </Flex>
       </MainLayout>
-   )
-}
-   
-export default HomePage
+   );
+};
+
+export default HomePage;
--- frontend/src/pages/HomePage/LandingPage.tsx
+++ frontend/src/pages/HomePage/LandingPage.tsx
@@ -3,26 +3,30 @@ import { Link } from 'react-router-dom';
 import { Button, Flex } from 'antd';
 import MainLayout from '../../app/MainLayout';
 import DeadlineChart from '../../features/deadlines/ui/DeadlineChart';
+import styles from './LangingPage.module.scss';
 
 const LandingPage: FC = () => {
    return (
       <MainLayout>
-         <Flex vertical gap={36}>
-            <h2 className='title'>Главная</h2>
-            <Flex vertical justify='center' align='center' gap='middle'>
+         <Flex vertical gap={36} className={styles.container}>
+            <h2 className="title">Главная</h2>
+            <Flex vertical justify="center" align="center" gap="middle">
                <h3>Следи за дедлайнами и не пропускай задачи</h3>
-               <h4>DevTracker поможет отследить прогресс, выстроить дисциплину и получить удовольствие</h4>
+               <h4>
+                  DevTracker поможет отследить прогресс, выстроить дисциплину и
+                  получить удовольствие
+               </h4>
                <Button color="default" variant="solid">
-                  <Link to='/auth'>Войти</Link>
+                  <Link to="/auth">Войти</Link>
                </Button>
             </Flex>
             <Flex vertical>
                <h3>Так может выглядеть твой прогресс</h3>
-               <DeadlineChart type='demo' />
+               <DeadlineChart type="demo" />
             </Flex>
          </Flex>
       </MainLayout>
-   )
-}
+   );
+};
 
-export default LandingPage
+export default LandingPage;
